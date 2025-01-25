68e9f8b4f0fc98a7c19936db626131837a7f6fdb
ziba iran <125620113+ziba-irani@users.noreply.github.com>
2025-01-26 01:52:14 +0330

    Update bite-better.tsx

--- src/components/welcome/bite-better.tsx
+++ src/components/welcome/bite-better.tsx
@@ -84,10 +84,10 @@ export default function BiteBetter() {
         <motion.div
           initial={{ x: -200 }}
           whileInView={{ x: 0 }}
-          transition={{ type: "tween", duration: "0.5" }}
-          className="md:col-span-2 col-span-3 flex-col items-center justify-center z-40"
+          transition={{ type: "tween", duration: 3 }} 
+          className="col-span-6 md:col-span-2 flex-col items-center justify-center z-40"
         >
-          <div className="w-full mx-auto  flex-col p-5 items-center justify-center content-center">
+          <div className="w-full mx-auto flex-col p-5 items-center justify-center content-center">
             {dataLeft.map((item) => (
               <CustomeCard
                 icon={item.icon}
@@ -103,8 +103,8 @@ export default function BiteBetter() {
         <motion.div
           initial={{ x: +200 }}
           whileInView={{ x: 0 }}
-          transition={{ type: "tween", duration: "0.5" }}
-          className="md:col-span-2 col-span-3 z-40"
+          transition={{ type: "tween", duration: "3" }}
+          className="md:col-span-2 hidden z-40"
         >
           <div className="w-full flex-col p-5 items-center justify-center content-center">
             {dataRight.map((item) => (
